0ca1bdd07894f7efdbb5df3686fc166b11264ca1
Pau Fracés <pfraces@gmail.com>
2015-05-17 22:02:55 +0200

    refactored install script

--- install.sh
+++ install.sh
@@ -1,52 +1,34 @@
 #!/bin/bash
 
-function install () {
-  mkdir -p "$HOME/$(dirname $1)" 2> /dev/null
-  cp "home/$1" "$HOME/$1"
+#
+# install-home
+# ============
+#
+# Selectively install config files
+#
+# Usage
+# -----
+#
+#     install-home foo bar qux ...
+#
+# If no arguments are passed, all config files are installed
+#
+#     install-home
+#
+
+function install-home () {
+  if test $# -eq 0
+  then
+    install-home $(find "home/" -type f | cut -sd / -f 2-)
+    exit
+  fi
+
+  while test $# -gt 0
+  do
+    mkdir -p "$HOME/$(dirname $1)" 2> /dev/null
+    cp "home/$1" "$HOME/$1"
+    shift
+  done
 }
 
-function install_all () {
-  install .bash_profile
-  install .bashrc
-  install .bashrc.aliases
-  install .bashrc.functions
-  install .bashrc.env
-
-  install .gitconfig
-  install .gitignore
-
-  install .i3/autostart
-  install .i3/config
-  install .i3status.conf
-
-  install .tmux.conf
-  install .tmuxline.conf
-  install .vimrc
-  install .local/share/fonts/PowerlineSymbols.otf
-  install .config/fontconfig/conf.d/10-powerline-symbols.conf
-
-  install .xinitrc
-  install .config/ranger/rc.conf
-
-  install bin/wifi
-  install bin/term
-  install bin/dualhead
-  install bin/triplehead
-
-  # archlinux
-  install .bashrc.archlinux
-  install .tmux.conf.archlinux
-  install bin/upd
-
-  # ubuntu
-  install .bashrc.ubuntu
-  install .local/share/applications/st.desktop
-}
-
-if test $# -eq 0
-then install_all && exit
-fi
-
-while test $# -gt 0
-do install "$1" && shift
-done
+install-home $@
