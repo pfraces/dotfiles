da9d74c81451393e7451e7ebdce29a52d01a0309
Pau Fracés <pau.fraces@scytl.com>
2016-01-11 12:27:29 +0100

    replaced multiline commands with singleline ones

--- base/bin/monitors.sh
+++ base/bin/monitors.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # home laptop (dual head)
-# xrandr --output LVDS-1 --auto --pos 0x0 \
+# xrandr --output LVDS-1 --auto --pos 0x0
 # xrandr --output VGA-1 --auto --left-of LVDS-1
 
 # home desktop (triple head)
-# xrandr --output HDMI-0 --auto --pos 0x0 \
-# xrandr --output DVI-1 --auto --right-of HDMI-0 \
+# xrandr --output HDMI-0 --auto --pos 0x0
+# xrandr --output DVI-1 --auto --right-of HDMI-0
 # xrandr --output DVI-0 --auto --right-of DVI-1
 
 # work (dual head)
-# xrandr --output VGA1 --auto --pos 0x0 \
+# xrandr --output VGA1 --auto --pos 0x0
 # xrandr --output DP1 --auto -left-of VGA1
